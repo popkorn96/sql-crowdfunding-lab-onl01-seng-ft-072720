e93fce72a6b2818abeda8f44f64bb6ba438ab415
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-26 00:51:13 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -57,5 +57,5 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
-  HAVING SUM("
+  WHERE projects.category = 'books'"
 end
